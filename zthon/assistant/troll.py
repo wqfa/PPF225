4adaff49995bc537798ee3dce77f756e3ae41dbc
XF <118357460+wqfa@users.noreply.github.com>
2023-08-08 10:52:34 +0300

    Update troll.py

--- zthon/assistant/troll.py
+++ zthon/assistant/troll.py
@@ -24,7 +24,7 @@ async def on_plug_in_callback_query_handler(event):
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
         except KeyError:
-            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات تــيبثون"
+            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات ريـدثـيون"
     else:
-        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات تيبثـون"
+        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات ريـدثـيون"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
