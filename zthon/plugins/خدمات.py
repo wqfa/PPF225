457dd50a0cb9e03428fe8517a8c43adcb89d439c
XF <118357460+wqfa@users.noreply.github.com>
2023-08-09 07:12:16 +0300

    Update خدمات.py

--- zthon/plugins/خدمات.py
+++ zthon/plugins/خدمات.py
@@ -139,14 +139,14 @@ async def _(event):
                 zilzal = zelzal.text
                 if "above 50MB" in zelzal.text:
                     aa = zilzal.replace(".apk filesize is above 50MB so you can download only using link", "**- حجم التطبيق اكبر من 50MB ؟!\n- قم بتحميل التطبيق عبـر البوت\n- ادخل للبوت @uploadbot وارسل الرابـط بالاسفـل**\n\n") 
-                    zz = aa.replace(" if you still want it as file copy the link and send to @UploadBot", "\n\n**- قنـاة السـورس : @ZedThon**") 
+                    zz = aa.replace(" if you still want it as file copy the link and send to @UploadBot", "\n\n**- قنـاة السـورس : @RedRheon**") 
                     await event.delete()
                     return await borg.send_message(event.chat_id, zz)
                 await event.delete()
                 await borg.send_file(
                     event.chat_id,
                     zelzal,
-                    caption=f"**{zelzal.text}\nBy: @ZedThon**",
+                    caption=f"**{zelzal.text}\nBy: @RedRheon**",
                 )
 
             else:
